19b731ee78504ce37ce450083122fb152d37fe31
rasmuscc <sexygaltab@gmail.com>
2020-01-27 15:20:55 +0100

    Added files - 1. commit!

---
+++ SSL/src/Server.java
@@ -0,0 +1,14 @@
+class Server {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+
+    private String padding (String src) {
+        return src;
+    }
+
+    private int listen (int port) {
+        return - 1;
+    }
+
+}
