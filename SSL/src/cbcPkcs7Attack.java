087f8ec1bc53a6b5bd94c4190e561a25c8c3e031
jesperjensen <Jesperjon@outlook.dk>
2020-03-25 22:25:30 +0100

    Refactoring and clean code (semi)

--- SSL/src/cbcPkcs7Attack.java
+++ SSL/src/cbcPkcs7Attack.java
@@ -7,11 +7,12 @@ class cbcPkcs7Attack {
 
     private Server server;
     private byte[] cipherText;
-    private byte[] fakeEnc;
+    private byte[] tempEnc;
     private byte[] encSubArr;
+    private int blockSize = 16;
     private int[] intermediate;
-    private int[] padI = new int[16];
-    private byte[] padDelta = new byte[16];
+    private int[] padI = new int[blockSize];
+    private byte[] padDelta = new byte[blockSize];
     private Scanner scanner;
     private int iter;
     private int block;
@@ -25,16 +26,20 @@ class cbcPkcs7Attack {
         scanner = new Scanner(System.in);
 
         cipherText = server.getCipherText();
-        encSubArr = Arrays.copyOfRange(cipherText.clone(), cipherText.length - 32, cipherText.length);
-        fakeEnc = Arrays.copyOfRange(cipherText.clone(), cipherText.length - 32, cipherText.length);
+
+        // make sub arrays for calculations
+        encSubArr = Arrays.copyOfRange(cipherText.clone(), cipherText.length - 2 * blockSize, cipherText.length);
+        tempEnc = Arrays.copyOfRange(cipherText.clone(), cipherText.length - 2 * blockSize, cipherText.length);
+
         intermediate = new int[encSubArr.length];
 
-        int numberOfBlocks = cipherText.length / 16;
+        int numberOfBlocks = cipherText.length / blockSize;
         block = 1;
 
         int startPos;
         iter = 1;
 
+        // begin plaintext recovery
         String plaintext = "";
         System.out.println("Encrypted message: " + new String(cipherText));
         System.out.println("First remove padding");
@@ -43,114 +48,120 @@ class cbcPkcs7Attack {
             if (scanner.nextLine().equals("no")) {
                 break;
             }
-            startPos = fakeEnc.length - iter;
+            startPos = tempEnc.length - iter;
+
             if (iter == 1 && block == 1) {
+                // first find out size of padding
                 getNextByte(startPos, iter);
                 System.out.println("Padding of size " + iter + " is now removed");
                 System.out.println("Press enter to continue or say no to exit");
             } else {
+                // update plaintext with next byte
                 plaintext = getNextByte(startPos, iter) + plaintext;
 
                 System.out.println("Recovered plaintext is now: " + plaintext);
                 System.out.println("Press enter to continue or say no to exit");
             }
             iter++;
-            if (iter == 17 && block + 1 != numberOfBlocks) {
+
+            if (iter == (blockSize + 1) && block + 1 != numberOfBlocks) {
+                // if end of block is reached make new temporary arrays and increase block number
                 block++;
-                fakeEnc = Arrays.copyOfRange(cipherText.clone(), cipherText.length - 16 - (block * 16), cipherText.length - ((block - 1) * 16));
-                encSubArr = Arrays.copyOfRange(cipherText.clone(), cipherText.length - 16 - (block * 16), cipherText.length - ((block - 1) * 16));
-                intermediate = new int[fakeEnc.length];
+                tempEnc = Arrays.copyOfRange(cipherText.clone(), cipherText.length - blockSize - (block * blockSize), cipherText.length - ((block - 1) * blockSize));
+                encSubArr = Arrays.copyOfRange(cipherText.clone(), cipherText.length - blockSize - (block * blockSize), cipherText.length - ((block - 1) * blockSize));
+                intermediate = new int[tempEnc.length];
+                // start over for next block
                 iter = 1;
-            } else if (iter == 17 && block + 1 == numberOfBlocks) {
+            } else if (iter == (blockSize + 1) && block + 1 == numberOfBlocks) {
+                // terminate if all blocks have been decrypted
                 System.out.println("Full plaintext recovered: " + plaintext);
                 break;
             }
         }
-
-
     }
 
     private String getNextByte(int pos, int iteration) {
 
         String res = "";
+        // check padding first
+        if (block == 1 && iteration == 1) {
 
-        for (int i = 0; i < 257; i++) {
-            byte[] temp = fakeEnc.clone();
-            temp[pos - 16] = (byte) i;
-            if (server.isPaddingCorrect(temp)) {
+            // get size of padding
+            int paddingSize = getPaddingSize(pos);
 
-                intermediate[pos] = (byte) ((byte) i ^ (byte) iteration);
-                if (intermediate[pos] < 0) {
-                    intermediate[pos] += 256;
-                }
-                padI[15] = (byte) intermediate[pos];
-                int paddingSize;
-                if (block == 1 && iteration == 1) {
-                    paddingSize = checkForPadding(pos, i + 1);
-                } else {
-                    paddingSize = 0;
-                }
+            // make intermediate array for padding
+            for (int j = 0; j < paddingSize; j++) {
+                padI[(blockSize - 1) - j] = (byte) (padDelta[(blockSize - 1) - j] ^ (byte) paddingSize);
+            }
 
-                if (paddingSize > 1) {
-                    for (int j = 0; j < paddingSize; j++) {
-                        padI[15 - j] = (byte) (padDelta[15 - j] ^ (byte) paddingSize);
-                    }
-                    System.arraycopy(padI, padI.length - paddingSize, intermediate, intermediate.length - paddingSize, paddingSize);
-                    fakeEnc = temp;
-                    for (int j = 0; j < paddingSize; j++) {
-                        fakeEnc[15 - j] = (byte) (padI[15 - j] ^ (byte) paddingSize + 1);
-                    }
-                    iter = paddingSize;
-                    break;
-                } else {
-                    fakeEnc = temp;
+            // insert padding intermediates into intermediate array
+            System.arraycopy(padI, padI.length - paddingSize, intermediate, intermediate.length - paddingSize, paddingSize);
+
+            // calculate new byte representation for next iteration
+            for (int j = 0; j < paddingSize; j++) {
+                tempEnc[(blockSize - 1) - j] = (byte) (padI[(blockSize - 1) - j] ^ (byte) paddingSize + 1);
+            }
+
+            // setting iter to padding size to skip padding
+            iter = paddingSize;
+        } else {
+            // guess byte to get a valid padding
+            for (int i = 0; i < 256; i++) {
+                byte[] temp = tempEnc.clone();
+                temp[pos - blockSize] = (byte) i;
+
+                if (server.isPaddingCorrect(temp)) {
+
+                    // calculate intermediate for pos
+                    intermediate[pos] = (byte) ((byte) i ^ (byte) iteration);
+
+                    // calculate new byte representation for next iteration
                     for (int j = 0; j < iteration; j++) {
-                        if (pos - 16 + j > -1) {
-                            fakeEnc[pos - 16 + j] = (byte) (intermediate[pos + j] ^ (byte) iteration + 1);
-                        }
+                        tempEnc[pos - blockSize + j] = (byte) (intermediate[pos + j] ^ (byte) iteration + 1);
                     }
-                }
-                res = decrypt((byte) pos, intermediate[pos]);
-                break;
 
+                    // get original plaintext byte
+                    res = decrypt((byte) pos, intermediate[pos]);
+                    break;
+                }
             }
         }
 
         return res;
     }
 
-    private int checkForPadding(int pos, int next) {
-        int paddingSize = -1;
-        boolean escape = false;
-        for (int j = 0; j < 16; j++) {
-            int start = 1;
-            if (j == 0) {
-                start = next;
-            }
-            for (int i = start; i < 257; i++) {
-                if (pos - 16 - j > -1) {
-                    byte[] temp = fakeEnc.clone();
-                    temp[pos - 16 - j] = (byte) i;
-                    if (server.isPaddingCorrect(temp)) {
-                        paddingSize = j + 1;
-                        padDelta[15 - j] = (byte) i;
-                        temp[pos - 16 - j] = (byte) 257;
-                        if (server.isPaddingCorrect(temp)) {
-                            System.out.println(paddingSize);
-                            paddingSize--;
-                            escape = true;
-                            break;
-                        }
-                        break;
-                    }
-                    if (i == 256){
-                        escape = true;
-                    }
-                }
-            }
-            if (escape) {
-                if (j == 1) paddingSize = 2;
+
+    /**
+     * Checks padding and returns the size
+     * @param pos position in arrays
+     * @return paddingSize
+     */
+    private int getPaddingSize(int pos) {
+        int paddingSize = 0;
+
+        // changing bytes to something illegal to see when padding is not valid
+        for (int j = (blockSize - 1); j > -1; j--) {
+            byte[] temp = tempEnc.clone();
+            temp[pos - blockSize - j] = (byte) 257;
+
+            // if not valid padding is broken and size is found
+            if (!server.isPaddingCorrect(temp)) {
+                paddingSize = j + 1;
                 break;
+            }
+        }
+
+        // getting deltas for padding
+        for (int j = 0; j < paddingSize; j++) {
+            for (int i = 0; i < 257; i++) {
+                byte[] temp = tempEnc.clone();
+                temp[pos - blockSize - j] = (byte) i;
+
+                // if valid then delta is i
+                if (server.isPaddingCorrect(temp)) {
+                    padDelta[(blockSize - 1) - j] = (byte) i;
+                    break;
+                }
             }
         }
 
@@ -159,7 +170,7 @@ class cbcPkcs7Attack {
 
 
     private String decrypt(int pos, int inter) {
-        return new String(new byte[] { (byte) (encSubArr[pos - 16] ^ inter) });
+        return new String(new byte[] { (byte) (encSubArr[pos - blockSize] ^ inter) });
     }
 
 }
